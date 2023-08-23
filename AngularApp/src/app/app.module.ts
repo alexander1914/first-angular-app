b62677697a5fbaae8c7d486296fe30fb311f4240
Alexander Oliveira <acc01719@sky.com.br>
2023-08-23 17:33:30 -0300

    Revisando os conceitos de routas e navegacao

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { rootRouterConfig } from './app.routes';
 
+// NgModule: é decorator para importar seus componentes dentro module
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    SobreComponent,
+    ContatoComponent
   ],
+  // imports: é uma propriedade para importar as routas do angular, bootstrap e etc. 
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(rootRouterConfig, { useHash: false })],
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ AngularApp/src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './navegacao/home/home.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+// import { DataBindingComponent } from './demos/data-binding/data-binding.component';
+// import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
+
+// Routes: objeto que serve para definir, as routas no seu projeto angular 
+
+export const rootRouterConfig: Routes = [
+    // path: é uma propriedade do objeto routes para mapear as routas
+    
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent},
+    { path: 'contato', component: ContatoComponent },
+    { path: 'sobre', component: SobreComponent },
+    // { path: 'feature-data-binding', component: DataBindingComponent },
+    // { path: 'produtos', component: ListaProdutoComponent },
+    // { path: 'produto-detalhe/:id', component: ListaProdutoComponent }
+];
