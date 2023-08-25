20af96e9624a1d44698fc8798ed1f66901f6972a
Alexander Oliveira <acc01719@sky.com.br>
2023-08-24 23:46:11 -0300

    Revisando o conceito de DataBinding

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
@@ -10,6 +11,7 @@ import { FooterComponent } from './navegacao/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
 import { rootRouterConfig } from './app.routes';
+import { DataBindingComponent } from './demos/data-binding/data-binding.component';
 
 // NgModule: é decorator para importar seus componentes dentro module
 @NgModule({
@@ -19,11 +21,13 @@ import { rootRouterConfig } from './app.routes';
     HomeComponent,
     FooterComponent,
     SobreComponent,
-    ContatoComponent
+    ContatoComponent,
+    DataBindingComponent
   ],
   // imports: é uma propriedade para importar as routas do angular, bootstrap e etc. 
   imports: [
     BrowserModule,
+    FormsModule,
     [RouterModule.forRoot(rootRouterConfig, { useHash: false })],
   ],
   providers: [
--- AngularApp/src/app/app.routes.ts
+++ AngularApp/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './navegacao/home/home.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
-// import { DataBindingComponent } from './demos/data-binding/data-binding.component';
+import { DataBindingComponent } from './demos/data-binding/data-binding.component';
 // import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
 
 // Routes: objeto que serve para definir, as routas no seu projeto angular 
@@ -14,7 +14,7 @@ export const rootRouterConfig: Routes = [
     { path: 'home', component: HomeComponent},
     { path: 'contato', component: ContatoComponent },
     { path: 'sobre', component: SobreComponent },
-    // { path: 'feature-data-binding', component: DataBindingComponent },
+    { path: 'feature-data-binding', component: DataBindingComponent },
     // { path: 'produtos', component: ListaProdutoComponent },
     // { path: 'produto-detalhe/:id', component: ListaProdutoComponent }
 ];
